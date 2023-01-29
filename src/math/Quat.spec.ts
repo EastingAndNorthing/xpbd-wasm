9e63dface67d4d8c7a5057f9a59b91a263e6dd99
Mark <markeasting@gmail.com>
2023-01-29 19:55:14 +0100

    Added Quat length and multiply tests

--- src/math/Quat.spec.ts
+++ src/math/Quat.spec.ts
@@ -2,7 +2,6 @@ import { Quat } from './Quat';
 import { x, y, z, w, eps } from '../test-constants.spec';
 
 describe('Quat', () => {
-
     test('Reference equality', () => {
         const ref = new Quat();
         expect<Quat>(ref).toBe(ref, 'Reference Equality');
@@ -30,5 +29,22 @@ describe('Quat', () => {
         expect(a.y).toBe(-b.y);
         expect(a.z).toBe(-b.z);
         expect(a.w).toBe(b.w);
+    });
+
+    test('length', () => {
+        const a = new Quat(x, y, z, w);
+        expect(a.length()).toBeCloseTo(7.348469257354736, 11);
+    });
+
+    describe('multiplyQuaternions/multiply', () => {
+        const q1 = new Quat(x, y, z, w);
+        const q2 = new Quat(0.5, 0.5, 0.5, 1.0);
+
+        const res = new Quat().multiplyQuaternions(q1, q2);
+
+        expect(res.x).toBe(4.0, 'x component');
+        expect(res.y).toBe(6.5, 'y component');
+        expect(res.z).toBe(6.0, 'z component');
+        expect(res.w).toBe(0.5, 'w component');
     });
 });

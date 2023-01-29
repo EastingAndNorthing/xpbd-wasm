d73eeb6e1aa32caf68e1286df343bbc834a7946f
Mark <markeasting@gmail.com>
2023-01-29 20:00:31 +0100

    Added simple benchmark

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,38 @@
-// The entry file of your WebAssembly module.
+import { Pose } from './body/Pose';
+import { Quat } from './math/Quat';
+import { Vec3 } from './math/Vec3';
 
-import { Vec3 } from "./math/Vec3";
+assert(ASC_FEATURE_SIMD, "expected SIMD enabled");
 
 export function add(a: i32, b: i32): i32 {
-  return a + b;
+    return a + b;
 }
 
-export function testModule(): f32 {
-  const v = new Vec3(0,0,0);
-  const v2 = new Vec3(0,5,0);
+export function benchmark(count: i32 = 10000): f64[] {
+    // const results: f64[] = [];
+    const results = new Array<f64>(count);
 
-  const dist = v.distanceTo(v2);
+    const p = new Pose(new Vec3(1, 2, 3), new Quat(1, 2, 3, 1).normalize());
+    
+    const v = new Vec3(2, 3, 4);
+    const q = new Quat(
+        2,
+        3,
+        4,
+        5
+    ).normalize();
 
-  return dist;
+    for (let i = 0; i < count; i++) {
+
+        p.transform(v);
+
+        v.add(new Vec3(1, 2, 3));
+
+        p.invTransform(v);
+
+        // results.push(v.applyQuaternion(q).x);
+        unchecked(results[i] = v.applyQuaternion(q).x);
+    }
+
+    return results;
 }

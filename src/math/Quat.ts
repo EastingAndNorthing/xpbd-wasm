3e821142e9afd1281465f68d601e27920c8a840c
Mark <markeasting@gmail.com>
2023-01-29 19:59:35 +0100

    Added SIMD instructions to Quat

--- src/math/Quat.ts
+++ src/math/Quat.ts
@@ -8,6 +8,13 @@ export class Quat {
         this.set(x, y, z, w);
     }
 
+    @inline
+    _extract(lane: v128): void {
+        this.x = f32x4.extract_lane(lane, 0);
+        this.y = f32x4.extract_lane(lane, 1);
+        this.z = f32x4.extract_lane(lane, 2);
+    }
+
     set(x: f32 = 0, y: f32 = 0, z: f32 = 0, w: f32 = 1): this {
         this.x = x;
         this.y = y;
@@ -33,12 +40,18 @@ export class Quat {
     /* Math (self) */
 
     lengthSq(): f32 {
-        return (
-            this.x * this.x +
-            this.y * this.y +
-            this.z * this.z +
-            this.w * this.w
-        );
+        // return (
+        //     this.x * this.x +
+        //     this.y * this.y +
+        //     this.z * this.z +
+        //     this.w * this.w
+        // );
+
+        const l1 = f32x4(this.x, this.y, this.z, this.w);
+        const l2 = f32x4(this.x, this.y, this.z, this.w);
+        const res = f32x4.mul(l1, l2);
+        
+        return this._sum(res);
     }
 
     length(): f32 {
@@ -97,16 +110,72 @@ export class Quat {
             qay = a.y,
             qaz = a.z,
             qaw = a.w;
+
         const qbx = b.x,
             qby = b.y,
             qbz = b.z,
             qbw = b.w;
 
-        this.x = qax * qbw + qaw * qbx + qay * qbz - qaz * qby;
-        this.y = qay * qbw + qaw * qby + qaz * qbx - qax * qbz;
-        this.z = qaz * qbw + qaw * qbz + qax * qby - qay * qbx;
-        this.w = qaw * qbw - qax * qbx - qay * qby - qaz * qbz;
+        // this.x = (qax * qbw) + (qaw * qbx) + (qay * qbz) - (qaz * qby);
+        // this.y = (qay * qbw) + (qaw * qby) + (qaz * qbx) - (qax * qbz);
+        // this.z = (qaz * qbw) + (qaw * qbz) + (qax * qby) - (qay * qbx);
+        // this.w = (qaw * qbw) - (qax * qbx) - qay * qby - qaz * qbz;
+
+        const lx1 = f32x4(qax, qaw, qay, qaz);
+        const lx2 = f32x4(qbw, qbx, qbz, qby);
+        const resX = f32x4.mul(lx1, lx2);
+
+        const ly1 = f32x4(qay, qaw, qaz, qax);
+        const ly2 = f32x4(qbw, qby, qbx, qbz);
+        const resY = f32x4.mul(ly1, ly2);
+
+        const lz1 = f32x4(qaz, qaw, qax, qay);
+        const lz2 = f32x4(qbw, qbz, qby, qbx);
+        const resZ = f32x4.mul(lz1, lz2);
+
+        const lw1 = f32x4(qaw, qax, qay, qaz);
+        const lw2 = f32x4(qbw, qbx, qby, qbz);
+        const resW = f32x4.mul(lw1, lw2);
+
+        this.x = (
+            f32x4.extract_lane(resX, 0) + 
+            f32x4.extract_lane(resX, 1) + 
+            f32x4.extract_lane(resX, 2) - 
+            f32x4.extract_lane(resX, 3)
+        );
+
+        this.y = (
+            f32x4.extract_lane(resY, 0) + 
+            f32x4.extract_lane(resY, 1) + 
+            f32x4.extract_lane(resY, 2) - 
+            f32x4.extract_lane(resY, 3)
+        );
+
+        this.z = (
+            f32x4.extract_lane(resZ, 0) + 
+            f32x4.extract_lane(resZ, 1) + 
+            f32x4.extract_lane(resZ, 2) - 
+            f32x4.extract_lane(resZ, 3)
+        );
+
+        this.w = (
+            f32x4.extract_lane(resW, 0) -
+            f32x4.extract_lane(resW, 1) - 
+            f32x4.extract_lane(resW, 2) - 
+            f32x4.extract_lane(resW, 3)
+        );
 
         return this;
     }
+
+    @inline
+    _sum(lane: v128): f32 {
+        return (
+            f32x4.extract_lane(lane, 0) + 
+            f32x4.extract_lane(lane, 1) + 
+            f32x4.extract_lane(lane, 2) + 
+            f32x4.extract_lane(lane, 3)
+        )
+    }
 }
+

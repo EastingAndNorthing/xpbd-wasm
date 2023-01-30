2a5a8d79fd40a1f7a861d4934327a38ff31c6399
Mark <markeasting@gmail.com>
2023-01-30 23:50:00 +0100

    Moved benchmarks

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,6 @@
-import { Pose } from './body/Pose';
-import { Quat } from './math/Quat';
 import { Vec3 } from './math/Vec3';
+import { Vec3 as Vec3SIMD } from './math/Vec3.simd';
 
-assert(ASC_FEATURE_SIMD, "expected SIMD enabled");
+assert(ASC_FEATURE_SIMD, "Expected SIMD to be enabled");
 
-export function add(a: i32, b: i32): i32 {
-    return a + b;
-}
-
-export function benchmark(count: i32 = 10000): f64[] {
-    // const results: f64[] = [];
-    const results = new Array<f64>(count);
-
-    const p = new Pose(new Vec3(1, 2, 3), new Quat(1, 2, 3, 1).normalize());
-    
-    const v = new Vec3(2, 3, 4);
-    const q = new Quat(
-        2,
-        3,
-        4,
-        5
-    ).normalize();
-
-    for (let i = 0; i < count; i++) {
-
-        p.transform(v);
-
-        v.add(new Vec3(1, 2, 3));
-
-        p.invTransform(v);
-
-        // results.push(v.applyQuaternion(q).x);
-        unchecked(results[i] = v.applyQuaternion(q).x);
-    }
-
-    return results;
-}
+export * from './benchmark/benchmark';

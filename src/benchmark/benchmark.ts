83c86496fd290f9b6069083a86dd34ad796e035f
markeasting <markeasting@gmail.com>
2023-01-31 20:03:57 +0100

    Tested a few benchmarks with pointers

--- src/benchmark/benchmark.ts
+++ src/benchmark/benchmark.ts
@@ -70,17 +70,14 @@ export function benchVec3SIMD(count: i32 = 100): boolean {
     // return result;
 }
 
-export function benchmarkIntegration(count: i32 = 100): f32[] {
+export function benchmarkIntegration(count: i32 = 100): usize {
 
-    // const arr = new ArrayBuffer(count);
-    // const view = new DataView(arr);
-
-    const results: f32[] = [];
-    // const results = new StaticArray<f32>(count);
+    const ptr: usize = 0;
+    const f32size = sizeof<f32>();
 
     const p = new Pose(new Vec3(1, 2, 3), new Quat(1, 2, 3, 1).normalize());
     
-    const v = new Vec3(2, 3, 4);
+    const v = new Vec3(1, 3, 4);
     const q = new Quat(
         2,
         3,
@@ -90,19 +87,23 @@ export function benchmarkIntegration(count: i32 = 100): f32[] {
 
     for (let i = 0; i < count; i++) {
 
-        p.transform(v);
+        // p.transform(v);
 
-        v.add(new Vec3(Mathf.random(), Mathf.random(), Mathf.random()));
+        // v.add(new Vec3(Mathf.random(), Mathf.random(), Mathf.random()));
 
-        p.invTransform(v);
+        // p.invTransform(v);
 
         // results.push(v.applyQuaternion(q).x);
-        unchecked(results[i] = v.applyQuaternion(q).x);
-
+        // unchecked(results[i] = v.applyQuaternion(q).x);
         // view.setFloat32(i, v.applyQuaternion(q).x);
+
+        p.p.add(v);
+
+        store<f32>(ptr + i * f32size, p.p.x);
     }
 
     // return view;
-    return results;
+    // return results;
+    return ptr;
 }
 

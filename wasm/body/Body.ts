998568a04ffba801ca718139430b61051d4d3f3c
markeasting <markeasting@gmail.com>
2023-02-04 13:03:22 +0100

    Added Body implementation

--- wasm/body/Body.ts
+++ wasm/body/Body.ts
@@ -1,234 +1,271 @@
 import { Collider } from '../collider/Collider';
 import { Pose } from './Pose';
-// import { Vec3 } from '../math/Vec3.simd';
-// import { Quat } from '../math/Quat.simd';
+import { Vec3 } from '../math/Vec3.simd';
+import { Quat } from '../math/Quat.simd';
+import { World } from '../core/World';
 
 export class Body {
 
-    public id: u32 = 0;
+    public id: i32 = 0;
 
     public collider: Collider;
 
     public pose: Pose = new Pose();
     public prevPose: Pose = new Pose();
 
-    // public vel = new Vec3(0.0, 0.0, 0.0);
-    // public omega = new Vec3(0.0, 0.0, 0.0);
+    public vel: Vec3 = new Vec3(0.0, 0.0, 0.0);
+    public omega: Vec3 = new Vec3(0.0, 0.0, 0.0);
 
-    // /* Pre velocity solve velocities */
-    // public velPrev = new Vec3(0.0, 0.0, 0.0);
-    // public omegaPrev = new Vec3(0.0, 0.0, 0.0);
+    /* Pre velocity solve velocities */
+    public velPrev: Vec3 = new Vec3(0.0, 0.0, 0.0);
+    public omegaPrev: Vec3 = new Vec3(0.0, 0.0, 0.0);
 
-    // // private mass = 1.0;
-    // private invMass = 1.0;
-    // private invInertia = new Vec3(1.0, 1.0, 1.0);
-    // public get mass() { return 1 / this.invMass; }
+    // private mass = 1.0;
+    private invMass: f32 = 1.0;
+    private invInertia: Vec3 = new Vec3(1.0, 1.0, 1.0);
+    public get mass(): f32 { return 1.0 / this.invMass; }
 
-    // public force = new Vec3();
-    // public torque = new Vec3();
-    // public gravity = 1.0;
+    public force: Vec3 = new Vec3();
+    public torque: Vec3 = new Vec3();
+    public gravity: f32 = 1.0;
 
-    // public bounciness = 0.5; // coefficient of restitution (e)
-    // public staticFriction = 0.5;
-    // public dynamicFriction = 0.3;
+    public bounciness: f32 = 0.5; // coefficient of restitution (e)
+    public staticFriction: f32 = 0.5;
+    public dynamicFriction: f32 = 0.3;
 
-    // public isDynamic = true;
+    public isDynamic: boolean = true;
 
-    // static maxRotationPerSubstep = 0.5;
-
-    constructor(collider: Collider) {
+    constructor(id: i32, collider: Collider) {
+        this.id = id;
         this.collider = collider;
+
+        this.readMemory();
 
         return this;
     }
 
-    // public addTo(world: World): this {
-    //     world.addBody(this);
+    public setID(id: i32): void {
+        this.id = id;
+    }
 
-    //     return this;
+    public setPos(x: f32, y: f32, z: f32): this {
+        this.pose.p.set(x, y, z);
+        this.prevPose.copy(this.pose);
+
+        this.writeMemory();
+
+        return this;
+    }
+
+    public setStatic(): this {
+        this.isDynamic = false;
+        this.gravity = 0.0;
+        this.invMass = 0.0;
+        this.invInertia = new Vec3(0.0);
+
+        this.updateCollider();
+
+        return this;
+    }
+
+    public setBox(size: Vec3, density = 1.0): this {
+        let mass = size.x * size.y * size.z * density;
+        this.invMass = 1.0 / mass;
+        mass /= 12.0;
+        this.invInertia.set(
+            1.0 / (size.y * size.y + size.z * size.z) / mass,
+            1.0 / (size.z * size.z + size.x * size.x) / mass,
+            1.0 / (size.x * size.x + size.y * size.y) / mass);
+
+        return this;
+    }
+
+    public getVelocityAt(pos: Vec3, usePrevVelocity: boolean = false): Vec3 {
+
+        if (!this.isDynamic)
+            return new Vec3(0, 0, 0);
+
+        const v = usePrevVelocity ? this.velPrev.clone() : this.vel.clone();
+        const o = usePrevVelocity ? this.omegaPrev.clone() : this.omega.clone();
+        const p = usePrevVelocity ? this.prevPose.p.clone() : this.pose.p.clone();
+
+        return Vec3.add(v, Vec3.cross(o, Vec3.sub(pos, p)));
+    }
+
+    public getInverseMass(normal: Vec3, pos: Vec3 | null = null): f32 {
+        if (!this.isDynamic)
+            return 0;
+
+        let n = new Vec3();
+
+        if (pos === null)
+            n.copy(normal);
+        else {
+            n.subVectors(pos, this.pose.p);
+            n.cross(normal);
+        }
+
+        this.pose.invRotate(n);
+        let w =
+            n.x * n.x * this.invInertia.x +
+            n.y * n.y * this.invInertia.y +
+            n.z * n.z * this.invInertia.z;
+
+        if (pos !== null && pos.length() > 0.00001)
+            w += this.invMass;
+
+        return w;
+    }
+
+    public applyCorrection(corr: Vec3, pos: Vec3 | null = null, velocityLevel: boolean = false): void {
+        if (!this.isDynamic)
+            return;
+
+        let dq = new Vec3();
+
+        if (pos === null)
+            dq.copy(corr);
+        else {
+            if (velocityLevel)
+                this.vel.addScaledVector(corr, this.invMass);
+            else
+                this.pose.p.addScaledVector(corr, this.invMass);
+
+            dq.subVectors(pos, this.pose.p);
+            dq.cross(corr);
+        }
+
+        this.pose.invRotate(dq);
+        dq.set(
+            this.invInertia.x * dq.x,
+            this.invInertia.y * dq.y,
+            this.invInertia.z * dq.z
+        );
+        this.pose.rotate(dq);
+
+        if (velocityLevel)
+            this.omega.add(dq);
+        else
+            this.applyRotation(dq);
+
+    }
+
+    public applyRotation(rot: Vec3, scale: f32 = 1.0): void {
+
+        // Safety clamping. This happens very rarely if the solver
+        // wants to turn the body by more than 30 degrees in the
+        // orders of milliseconds
+        let maxPhi: f32 = 0.5;
+        let phi = rot.length();
+        if (phi * scale > maxPhi)
+            scale = maxPhi / phi;
+
+        let dq = new Quat(
+            rot.x * scale,
+            rot.y * scale,
+            rot.z * scale,
+            0.0
+        );
+        dq.multiply(this.pose.q);
+
+        this.pose.q.set(
+            this.pose.q.x + 0.5 * dq.x,
+            this.pose.q.y + 0.5 * dq.y,
+            this.pose.q.z + 0.5 * dq.z,
+            this.pose.q.w + 0.5 * dq.w
+        );
+        this.pose.q.normalize();
+    }
+
+    public integrate(dt: f32, gravity: Vec3): void {
+        if (!this.isDynamic)
+            return;
+
+        this.prevPose.copy(this.pose);
+
+        this.vel.add(Vec3.mul(gravity, this.gravity * dt));
+        this.vel.add(Vec3.mul(this.force, this.invMass * dt));
+        this.omega.addScaledVector(this.torque.clone().multiply(this.invInertia), dt);
+        this.pose.p.addScaledVector(this.vel, dt);
+        this.applyRotation(this.omega, dt);
+
+    }
+
+    public update(dt: f32): void {
+        if (!this.isDynamic)
+            return;
+
+        this.velPrev.copy(this.vel);
+        this.omegaPrev.copy(this.omega);
+
+        this.vel.subVectors(this.pose.p, this.prevPose.p);
+        this.vel.multiplyScalar(1.0 / dt);
+
+        let dq = new Quat;
+        dq.multiplyQuaternions(this.pose.q, this.prevPose.q.conjugate());
+
+        this.omega.set(
+            dq.x * 2.0 / dt,
+            dq.y * 2.0 / dt,
+            dq.z * 2.0 / dt
+        );
+
+        if (dq.w < 0.0)
+            this.omega.set(-this.omega.x, -this.omega.y, -this.omega.z);
+
+        if (this.vel.length() < 0.005)
+            this.vel.multiplyScalar(0);
+
+        if (this.omega.length() < 0.005)
+            this.omega.multiplyScalar(0);
+
+        this.updateCollider();
+    }
+
+    // @TODO move to XPBD browser/client side
+    // public applyForceW(force: Vec3, worldPos: Vec3 = new Vec3(0,0,0)) {
+    //     const F = force.clone();
+
+    //     this.force.add(F);
+    //     this.torque.add(F.cross(this.pose.p.clone().sub(worldPos)))
     // }
 
-    // public setPos(x: number, y: number, z: number): this {
-    //     this.pose.p.set(x, y, z);
+    @inline
+    public updateCollider(): void {
+        this.collider.updateRotation(this.pose.q);
+    }
 
-    //     return this;
-    // }
+    @inline
+    private getAddress(): usize {
+        store<f32>(this.id * sizeof<f32>(), f32(this.id));
 
-    // public setStatic(): this {
-    //     this.isDynamic = false;
-    //     this.gravity = 0.0;
-    //     this.invMass = 0.0;
-    //     this.invInertia = new Vec3(0.0);
+        return World.bodyPtr * sizeof<f32>() + (this.id * sizeof<f32>() * 7);
+    }
 
-    //     this.updateCollider();
+    @inline
+    public readMemory(): void {
+        const offset = this.getAddress();
+        this.pose.p.x = load<f32>(offset + sizeof<f32>() * 0);
+        this.pose.p.y = load<f32>(offset + sizeof<f32>() * 1);
+        this.pose.p.z = load<f32>(offset + sizeof<f32>() * 2);
+        this.pose.q.x = load<f32>(offset + sizeof<f32>() * 3);
+        this.pose.q.y = load<f32>(offset + sizeof<f32>() * 4);
+        this.pose.q.z = load<f32>(offset + sizeof<f32>() * 5);
+        this.pose.q.w = load<f32>(offset + sizeof<f32>() * 6);
 
-    //     return this;
-    // }
+        this.prevPose.copy(this.pose);
+    }
 
-    // public setBox(size: Vec3, density = 1.0): this {
-    //     let mass = size.x * size.y * size.z * density;
-    //     this.invMass = 1.0 / mass;
-    //     mass /= 12.0;
-    //     this.invInertia.set(
-    //         1.0 / (size.y * size.y + size.z * size.z) / mass,
-    //         1.0 / (size.z * size.z + size.x * size.x) / mass,
-    //         1.0 / (size.x * size.x + size.y * size.y) / mass);
-
-    //     return this;
-    // }
-
-    // public getVelocityAt(pos: Vec3, usePrevVelocity = false): Vec3 {
-
-    //     if (!this.isDynamic)
-    //         return new Vec3(0, 0, 0);
-
-    //     const v = usePrevVelocity ? this.velPrev.clone() : this.vel.clone();
-    //     const o = usePrevVelocity ? this.omegaPrev.clone() : this.omega.clone();
-    //     const p = usePrevVelocity ? this.prevPose.p.clone() : this.pose.p.clone();
-
-    //     return Vec3.add(v, Vec3.cross(o, Vec3.sub(pos, p)));
-    // }
-
-    // public getInverseMass(normal: Vec3, pos: Vec3 | null = null): number {
-    //     if (!this.isDynamic)
-    //         return 0;
-
-    //     let n = new Vec3();
-
-    //     if (pos === null)
-    //         n.copy(normal);
-    //     else {
-    //         n.subVectors(pos, this.pose.p);
-    //         n.cross(normal);
-    //     }
-
-    //     this.pose.invRotate(n);
-    //     let w =
-    //         n.x * n.x * this.invInertia.x +
-    //         n.y * n.y * this.invInertia.y +
-    //         n.z * n.z * this.invInertia.z;
-
-    //     if (pos !== null && pos.length() > 0.00001)
-    //         w += this.invMass;
-
-    //     return w;
-    // }
-
-    // public applyCorrection(corr: Vec3, pos: Vec3 | null = null, velocityLevel = false): void {
-    //     if (!this.isDynamic)
-    //         return;
-
-    //     let dq = new Vec3();
-
-    //     if (pos === null)
-    //         dq.copy(corr);
-    //     else {
-    //         if (velocityLevel)
-    //             this.vel.addScaledVector(corr, this.invMass);
-    //         else
-    //             this.pose.p.addScaledVector(corr, this.invMass);
-
-    //         dq.subVectors(pos, this.pose.p);
-    //         dq.cross(corr);
-    //     }
-
-    //     this.pose.invRotate(dq);
-    //     dq.set(
-    //         this.invInertia.x * dq.x,
-    //         this.invInertia.y * dq.y,
-    //         this.invInertia.z * dq.z
-    //     );
-    //     this.pose.rotate(dq);
-
-    //     if (velocityLevel)
-    //         this.omega.add(dq);
-    //     else
-    //         this.applyRotation(dq);
-
-    // }
-
-    // public applyRotation(rot: Vec3, scale: number = 1.0): void {
-
-    //     // Safety clamping. This happens very rarely if the solver
-    //     // wants to turn the body by more than 30 degrees in the
-    //     // orders of milliseconds
-    //     let maxPhi = 0.5;
-    //     let phi = rot.length();
-    //     if (phi * scale > maxPhi)
-    //         scale = maxPhi / phi;
-
-    //     let dq = new Quat(
-    //         rot.x * scale,
-    //         rot.y * scale,
-    //         rot.z * scale,
-    //         0.0
-    //     );
-    //     dq.multiply(this.pose.q);
-
-    //     this.pose.q.set(
-    //         this.pose.q.x + 0.5 * dq.x,
-    //         this.pose.q.y + 0.5 * dq.y,
-    //         this.pose.q.z + 0.5 * dq.z,
-    //         this.pose.q.w + 0.5 * dq.w
-    //     );
-    //     this.pose.q.normalize();
-    // }
-
-    // public integrate(dt: number, gravity: Vec3): void {
-    //     if (!this.isDynamic)
-    //         return;
-
-    //     this.prevPose.copy(this.pose);
-
-    //     this.vel.add(Vec3.mul(gravity, this.gravity * dt));
-    //     this.vel.add(Vec3.mul(this.force, this.invMass * dt));
-    //     this.omega.addScaledVector(this.torque.clone().multiply(this.invInertia), dt);
-    //     this.pose.p.addScaledVector(this.vel, dt);
-    //     this.applyRotation(this.omega, dt);
-
-    // }
-
-    // public update(dt: number): void {
-    //     if (!this.isDynamic)
-    //         return;
-
-    //     this.velPrev.copy(this.vel);
-    //     this.omegaPrev.copy(this.omega);
-
-    //     this.vel.subVectors(this.pose.p, this.prevPose.p);
-    //     this.vel.multiplyScalar(1.0 / dt);
-
-    //     let dq = new Quat;
-    //     dq.multiplyQuaternions(this.pose.q, this.prevPose.q.conjugate());
-
-    //     this.omega.set(
-    //         dq.x * 2.0 / dt,
-    //         dq.y * 2.0 / dt,
-    //         dq.z * 2.0 / dt
-    //     );
-
-    //     if (dq.w < 0.0)
-    //         this.omega.set(-this.omega.x, -this.omega.y, -this.omega.z);
-
-    //     if (this.vel.length() < 0.005)
-    //         this.vel.multiplyScalar(0);
-
-    //     if (this.omega.length() < 0.005)
-    //         this.omega.multiplyScalar(0);
-
-    //     this.updateCollider();
-    // }
-
-    // // @TODO move to XPBD browser/client side
-    // // public applyForceW(force: Vec3, worldPos: Vec3 = new Vec3(0,0,0)) {
-    // //     const F = force.clone();
-
-    // //     this.force.add(F);
-    // //     this.torque.add(F.cross(this.pose.p.clone().sub(worldPos)))
-    // // }
-
-    // public updateCollider() {
-    //     this.collider.updateRotation(this.pose.q);
-    // }
+    @inline
+    public writeMemory(): void {
+        const offset = this.getAddress();
+        store<f32>(offset + sizeof<f32>() * 0, this.pose.p.x);
+        store<f32>(offset + sizeof<f32>() * 1, this.pose.p.y);
+        store<f32>(offset + sizeof<f32>() * 2, this.pose.p.z);
+        store<f32>(offset + sizeof<f32>() * 3, this.pose.q.x);
+        store<f32>(offset + sizeof<f32>() * 4, this.pose.q.y);
+        store<f32>(offset + sizeof<f32>() * 5, this.pose.q.z);
+        store<f32>(offset + sizeof<f32>() * 6, this.pose.q.w);
+    }
 
 }
